a7c0651c26a22064ba2c59cb8ab4d8b55dbd0e72
Walter Winser <turtle89431@gmail.com>
2020-12-04 19:54:32 -0800

    css fixes

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,6 +1,5 @@
 <script>
   import Nav from "../components/Nav.svelte";
-
   export let segment;
 </script>
 
@@ -8,9 +7,17 @@
   * {
     margin: 0;
     padding: 0;
+    box-sizing: border-box;
+  }
+  main {
+    position: absolute;
+    top: 0;
+    height: calc(100vh - 2em); /* hieght must me 100vh - hieght of nav */
+    box-sizing: border-box;
   }
 </style>
 
 <Nav {segment} />
-
-<slot />
+<main>
+  <slot />
+</main>
